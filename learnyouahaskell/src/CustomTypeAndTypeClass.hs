b553119db030a06a742bd43cc1193f79df33c002
Ryan N <rblnguyen@gmail.com>
2018-01-24 18:47:14 -0700

    Recursive Type

--- learnyouahaskell/src/CustomTypeAndTypeClass.hs
+++ learnyouahaskell/src/CustomTypeAndTypeClass.hs
@@ -110,6 +110,28 @@ lockerLookup lockerNumber maps =
                             else Left $ "Locker " ++ show lockerNumber ++ " is already taken!"
 
 --let lockers = Map.fromList[(100,(Taken,"ZD39I")),(101,(Free,"JAH3I")),(103,(Free,"IQSA9")),(105,(Free,"QOTSA")),(109,(Taken,"893JJ")),(110,(Taken,"99292"))]  
+--- Recursive Data Structure ---
+data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+--data List' a = Empty' | Cons' {listHead :: a, listTail :: List' a} deriving (Show, Read, Eq, Ord)
+
+-- Binary Search tree
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right) 
+              | x == a = Node x left right
+              | x < a = Node a (treeInsert x left) right
+              | x > a = Node a left (treeInsert x right)
+
+
+
+
+
+
 
 ---- JSON 
 data JValue = JString String
